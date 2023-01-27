e38cc316a151aa80f7ab9c96cd2f1692f84a05e3
Gorazd <gorazdko@gmail.com>
2023-01-27 01:18:06 +0100

    try unit testing

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -16,6 +16,7 @@ use defmt_rtt as _; // global logger
 use embassy_nrf as _; // memory layout
 use panic_probe as _;
 
+
 // same panicking *behavior* as `panic-probe` but doesn't print a panic message
 // this prevents the panic message being printed *twice* when `defmt::panic` is invoked
 #[defmt::panic_handler]
@@ -30,16 +31,29 @@ pub fn exit() -> ! {
     }
 }
 
+
+extern {
+    fn hello() -> i32;
+}
+
+
 // defmt-test 0.3.0 has the limitation that this `#[tests]` attribute can only be used
 // once within a crate. the module can be in any file but there can only be at most
 // one `#[tests]` module in this library crate
+// cargo test --lib
 #[cfg(test)]
 #[defmt_test::tests]
 mod unit_tests {
     use defmt::assert;
+    use super::*;
 
     #[test]
     fn it_works() {
         assert!(true)
+    }
+
+    #[test]
+    fn test_hello() {
+        assert!(unsafe {hello()} == 42);
     }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -5,6 +5,7 @@ use ble_async_demo as _; // memory layout + panic handler
 
 // See https://crates.io/crates/defmt-test/0.3.0 for more documentation (e.g. about the 'state'
 // feature)
+// cargo test --test integration
 #[defmt_test::tests]
 mod tests {
     use defmt::assert;
